c3c6c65c196e5fcbcd5981e6a0beaaed685d515f
timbynce <cherar90@gmail.com>
2022-04-07 10:55:31 +0300

    Изменил цикл по подобию слайда из учебных материалов

--- lesson2/six.rb
+++ lesson2/six.rb
@@ -1,17 +1,17 @@
 purchases={}
 name = nil
 sum = 0
-until name == "Стоп"
+loop do
     puts "Введите название товара"
     name = gets.chomp
-    if name != "Стоп"
-        puts "Введите его цену"
-        price = gets.chomp.to_f
-        puts "Какое число #{name} вы купили?"
-        quantity = gets.chomp.to_i
-        sum_position = (price * quantity)
-        purchases[name] = {price: price, quantity: quantity, sum_position: sum_position }        
-    end
+    break if name == "Стоп"
+    puts "Введите его цену"
+    price = gets.chomp.to_f
+    puts "Какое число #{name} вы купили?"
+    quantity = gets.chomp.to_i
+    sum_position = (price * quantity)
+    purchases[name] = {price: price, quantity: quantity, sum_position: sum_position }        
+    
 end
 
 total = "%0.2f" % (purchases.each_value.reduce(0) { |k,v| k + v[:sum_position]})
